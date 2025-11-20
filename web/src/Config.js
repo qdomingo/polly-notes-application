37eff083d465a424b446f7c1a2c457ac93b5ad83
qdomingo <qdomingo@hotmail.com>
2025-11-20 23:24:48 +0100

    ajuste variables en config.js

--- web/src/Config.js
+++ web/src/Config.js
@@ -1,7 +1,7 @@
-//Variables to replace
-const poolId = 'us-east-1_KkGhhyQlv'
-const clientId = '7cgtlu2169gicr7k9fnis5i2i2'
-const api = 'https://dish7tdsrf.execute-api.us-east-1.amazonaws.com/Prod'
+// Variables to replace
+const poolId = '[UserPoolId]'
+const clientId = '[AppClientId]'
+const api = '[ApiURL]'
 
 const Config = {
     UserPoolId: poolId,
